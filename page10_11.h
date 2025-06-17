9de19a1a199c119a98282d99b86e125beaa2c719
paprika8 <aserstritt@gmail.com>
2025-06-17 15:13:24 +0800

    fix

--- page10_11.h
+++ page10_11.h
@@ -168,8 +168,6 @@ namespace Graphs
 			input->paint(hdc);
 			};
 		check_but2->click = [=](Button*)->void {
-			if(tx->gr.nodes.empty())
-					return;
 			std::wstring str = input->text;
 
 			std::vector<int> res = parsing10_11(str);
